885a4fc0bd66ae930b8e07324e8ec6acdfc26213
Master Jung <ajdkfl6445@gmail.com>
2018-11-07 00:44:39 +0900

    20181106 study

---
+++ Frist/src/chapter2/TypeInference.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class TypeInference {
+
+	public static void main(String[] args) {
+		var num = 10;
+		var dnum = 10.0;
+		var str = "hello";
+		
+		System.out.println(num);
+		System.out.println(dnum);
+		System.out.println(str);
+	}
+
+}
