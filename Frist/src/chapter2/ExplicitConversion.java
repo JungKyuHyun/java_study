f3211a7cde46e3f14bcb3a80817a19b1f051791f
Master Jung <ajdkfl6445@gmail.com>
2018-11-10 13:41:00 +0900

    20181110 study2

---
+++ Frist/src/chapter2/ExplicitConversion.java
@@ -0,0 +1,27 @@
+package chapter2;
+
+public class ExplicitConversion {
+
+	public static void main(String[] args) {
+		double dNum1 = 1.2;
+		float fNum2 = 0.9F;
+		
+		int iNum3 = (int)dNum1 + (int)fNum2; // 1
+		int iNum4 = (int)(dNum1 + fNum2); // 2
+		System.out.println(iNum3);
+		System.out.println(iNum4);
+		
+		int iNum5 = 10;
+		float fNum6 = 2.0f;
+		
+		System.out.println(iNum5+(int)fNum6);
+		System.out.println(iNum5-(int)fNum6);
+		System.out.println(iNum5*(int)fNum6);
+		System.out.println(iNum5/(int)fNum6);
+		
+		char ch1 = '±Û';
+		System.out.println((int)ch1);
+		
+	}
+
+}
